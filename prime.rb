906f8ac687b0cfa4be9982c04ea5b4cc06fa177c
Will Carter <will.carter@fergusllc.com>
2020-01-06 00:59:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,62 @@
-def prime?(num)
-  if num < 2 then return false end
-  for i in 2..num-1 do
-    if num.to_f % i == 0 then return false end
-  end
-  true
-end
+# def prime?(num)
+#   if num < 2 then return false end
+#   for i in 2..num-1 do
+#     if num.to_f % i == 0 then return false end
+#   end
+#   true
+# end
 
-# puts prime?(2)
-# puts prime?(3)
+# def prime?(num)
+#   if num < 2
+#     false
+#   else
+#     (2..num-1).to_a.map do |possible_factor|
+#       puts "#{possible_factor} : #{num % possible_factor}"
+#       if num % possible_factor == 0
+#         return false
+#       end
+#     end
+#     true
+#   end
+# end
+
+def prime_tester(num)
+  (2..num-1).to_a.map { |possible_factor|
+    puts "#{possible_factor} : #{num % possible_factor}"
+  }
+end
+prime_tester(11)
+
+# def prime_tester2(num)
+#   (2..num-1).to_a.map{|possible_factor| possible_factor}
+#     puts "---"
+#   end
+# end
+# prime_tester2(11)
+
+# def prime?(num)
+#   if num < 2
+#     false
+#   else
+#     (2..num-1).to_a.map(&:possible_factor)
+#       puts "#{:possible_factor} : #{num % :possible_factor}"
+#       if num % possible_factor == 0
+#         return false
+#       end
+#     end
+#     true
+#   end
+# end
+
+
+# (2..num-1).to_a.all? do |possible_factor|
+#   num % possible_factor != 0
+# end
+
+#puts prime?(2)
+#puts prime?(3)
 # puts prime?(4)
-# puts prime?(5)
+#puts prime_tester(11)
 # puts prime?(6)
 # puts prime?(7)
 # puts prime?(8)
